3693212a2f4d9fc0fa9b23f7e798384060d889e0
Cel <celinenico22@gmail.com>
2021-02-25 18:45:52 -0500

    commit tree 2f1eebbe105fb19e590034608e9e53d834875067

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+
+def prime?(integer)
+    if integer < 0 or integer == 0 or integer == 1
+        return false    
+    else 
+        (2..integer-1).to_a.all? do |factor|
+            integer % factor != 0
+        end 
+    end
+end 
+
+puts prime?(10344)  
